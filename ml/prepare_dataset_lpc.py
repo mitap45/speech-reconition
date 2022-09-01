9ec9a1026ca17b58d14e0885f537c1b7a736f153
Muratcan İtap <mitap45@gmail.com>
2022-09-01 17:19:54 +0300

    lpc added

---
+++ ml/prepare_dataset_lpc.py
@@ -0,0 +1,63 @@
+import librosa
+import os
+import json
+import warnings
+
+warnings.filterwarnings("ignore")
+
+DATASET_PATH = "dataset_turkish"
+JSON_PATH = "data_turkish_lpc.json"
+SAMPLES_TO_CONSIDER = 22050 # 1 sec
+
+def prepare_dataset(dataset_path, json_path, order=13, hop_length=512, n_fft=2048):
+
+    # data dictionary
+    data = {
+        "mappings": [],
+        "labels": [],
+        "LPCs": [],
+        "files": []
+
+    }
+
+    # loop through all the sub dirs
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+
+        # we need to ensure that we're not at root level
+        if dirpath is not dataset_path:
+
+            # update mappings
+            category = dirpath.split("/")[-1] # dataset/down -> [dataset, down]
+            data["mappings"].append(category)
+            print(f"Processing {category}")
+
+            # loop through all the filenames and extract MFCCs
+            for f in filenames:
+
+                # get the file path
+                file_path = os.path.join(dirpath, f)
+
+                # load the audio file
+                signal, sr = librosa.load(file_path)
+
+                # ensure the audio file is at least 1 sec
+                if len(signal) >= SAMPLES_TO_CONSIDER:
+
+                    # enforce 1 sec. long signal
+                    signal = signal[:SAMPLES_TO_CONSIDER]
+
+                    # extract the LPCs
+                    LPCs = librosa.lpc(signal, order=order)
+
+                    # store data
+                    data["labels"].append(i-1)
+                    data["LPCs"].append(LPCs.T.tolist())
+                    data["files"].append(file_path)
+                    print(f"{file_path} : {i-1}")
+
+    with open(json_path, "w") as fp:
+        json.dump(data, fp, indent=4)
+
+if __name__ == "__main__":
+
+    prepare_dataset(DATASET_PATH, JSON_PATH)
